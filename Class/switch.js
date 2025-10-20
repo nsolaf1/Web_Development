a24440c756a9b48032552ac7591ff0b7eaade91d
NSOLAf1 <nsolaf1@gmail.com>
2025-10-20 15:52:31 +0600

    l8

--- Class/switch.js
+++ Class/switch.js
@@ -23,36 +23,57 @@
 //         console.log("Grade A")
 //         break;
 //     case(score >=80 ):
+//       console.log("Grade A-")
 //     case(score >=70 ):
-//         console.log("Grade B")
 //         console.log("Grade B")
 //         break;
 //     default:
 //         console.log("Grade C")
 // }
 
-let num1 = 1;
-let num2 = 5;
-let opperator = "+";
+// let num1 = 1;
+// let num2 = 5;
+// let opperator = "+";
 
-if (opperator == "+") {
+// if (opperator == "+") {
+//   console.log("sum :", num1 + num2);
+// } else if (opperator == "-") {
+//   console.log("sum :", num1 - num2);
+// } else if (opperator == "*") {
+//   console.log("sum :", num1 * num2);
+// } else if (opperator == "/") {
+//   console.log("sum :", num1 / num2);
+// } else {
+//   console.log("Invalid Input");
+// }
 
-  console.log("sum :", num1 + num2);
+// let num1 = 5;
+// let num2 = 4;
+// let opperator = '++'
+// let result;
 
-} else if (opperator == "-") {
+// switch(opperator){
+//   case '+':
+//     result = num1 + num2;
+//     break;
+//   case '-':
+//     result = num1 - num2;
+//     break;
+//   case '/':
+//     result = num1 / num2;
+//     break;
+//   case '*':
+//     result = num1 * num2;
+//     break;
+//   default:
+//     result = "Error"
+// }
+// console.log(result)
 
-  console.log("sum :", num1 + num2);
 
-} else if (opperator == "*") {
 
-  console.log("sum :", num1 * num2);
-
-} else if (opperator == "/") {
-
-  console.log("sum :", num1 / num2);
-
-} else {
-
-  console.log("Invalid Input");
-
+// let num = 0;
+let num = 3; 
+for(let i = 1; i <= 10; i++ ){
+  console.log(`${num} x ${i} = ${num*i}`);
 }
