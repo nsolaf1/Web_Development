98404828c4d6a94dfae1fcc704c458a2a1876553
NSOLAf1 <nsolaf1@gmail.com>
2025-10-27 15:45:23 +0600

    array

---
+++ Class/array.js
@@ -0,0 +1,19 @@
+const cars = ["BMW", "YOYOTA", "Lexus", "Honda", "FIT"];
+
+// console.log(cars.length);
+
+// for(let i = 0; i < cars.length; i++){
+//     console.log(cars[i]);
+
+//     for(let j = 0; j < cars[i].length; j++){
+//         console.log(cars[i][j])
+
+//     }
+    
+// }
+let num = 3; 
+for(let i = 1; i <= 10; i++ ){
+  console.log(`${num} x ${i} = ${num*i}`);
+}
+
+
