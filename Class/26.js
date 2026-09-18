3c05b6b1744a69e73beb0c811fe758b8ce8c9705
NSOLAf1 <nsolaf1@gmail.com>
2026-09-18 11:02:27 +0600

    asdasd

---
+++ Class/26.js
@@ -0,0 +1,17 @@
+// let score = 105;
+
+// if(score > 100) {
+//     console.log("Error")
+// } else if(score >= 90){
+//     console.log("A");
+// } else if (score >= 75){
+//     console.log("C");
+// } else {
+//     console.log("F")
+// }
+// console.log("END")
+
+for (let i = 0; i < 5; i++){
+    console.log("|Hello")
+}
+
--- Class/script.js
+++ Class/script.js
@@ -1,3 +1,4 @@
 alert("ERROR");
-console.log("THIS is console")
+
+// console.log("THIS is console")
 // console.log("THIS is consol1e")
